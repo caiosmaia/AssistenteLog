0bcb22ed087135da754c311d11e13f6cc11864c4
caiosmaia <37799921+caiosmaia@users.noreply.github.com>
2024-04-22 23:04:31 -0300

    Update testes.py

--- assistente_virtualLOG/assistente virtual/testes.py
+++ assistente_virtualLOG/assistente virtual/testes.py
@@ -1,10 +1,15 @@
 import unittest
 from assistente import *
 
-CHAMANDO_JOANA = "/misc/ifba/workspaces/inteligencia artificial/assistente virtual/audios/chamando joana.wav"
-CHAMANDO_MARIA = "/misc/ifba/workspaces/inteligencia artificial/assistente virtual/audios/chamando outro nome.wav"
-LIGAR_LAMPADA = "/misc/ifba/workspaces/inteligencia artificial/assistente virtual/audios/ligar lampada.wav"
-DESLIGAR_LAMPADA = "/misc/ifba/workspaces/inteligencia artificial/assistente virtual/audios/desligar lampada.wav"
+CHAMANDO_LOG = "C:/Users/caiom/OneDrive/Área de Trabalho/assistente_virtualLOG/assistente virtual/audios/LogNome.wav"
+VERIFICAR_ROTA = "C:/Users/caiom/OneDrive/Área de Trabalho/assistente_virtualLOG/assistente virtual/audios/VerificarRota.wav"
+ANALISAR_ROTA = "C:/Users/caiom/OneDrive/Área de Trabalho/assistente_virtualLOG/assistente virtual/audios/AnalisarRotas.wav"
+MARCAR_MANUTENCAO = "C:/Users/caiom/OneDrive/Área de Trabalho/assistente_virtualLOG/assistente virtual/audios/MarcarManutencao.wav"
+AGENDAR_MANUTENCAO = "C:/Users/caiom/OneDrive/Área de Trabalho/assistente_virtualLOG/assistente virtual/audios/AgendarManutencao.wav"
+ENCONTRAR_VEICULO = "C:/Users/caiom/OneDrive/Área de Trabalho/assistente_virtualLOG/assistente virtual/audios/EncontrarVeiculo.wav"
+LOCALIZAR_VEICULO = "C:/Users/caiom/OneDrive/Área de Trabalho/assistente_virtualLOG/assistente virtual/audios/LocalizarVeiculo.wav"
+FAZER_RELATORIO = "C:/Users/caiom/OneDrive/Área de Trabalho/assistente_virtualLOG/assistente virtual/audios/FazerRelatorio.wav"
+GERAR_RELATORIO = "C:/Users/caiom/OneDrive/Área de Trabalho/assistente_virtualLOG/assistente virtual/audios/GerarRelatorio.wav"
 
 class TesteNomeAssistente(unittest.TestCase):
     
@@ -12,7 +17,7 @@ class TesteNomeAssistente(unittest.TestCase):
         self.iniciado, self.reconhecedor, _, self.nome_do_assistente, _ = iniciar()
 
     def testar_01_reconhecer_nome(self):
-        tem_transcricao, transcricao = processar_audio_de_teste(CHAMANDO_JOANA, self.reconhecedor)
+        tem_transcricao, transcricao = processar_audio_de_teste(CHAMANDO_LOG, self.reconhecedor)
 
         self.assertTrue(tem_transcricao)
 
@@ -20,22 +25,13 @@ class TesteNomeAssistente(unittest.TestCase):
         self.assertIsNotNone(tokens)
         self.assertEqual(tokens[0], self.nome_do_assistente)
 
-    def testar_02_nao_reconhecer_outro_nome(self):
-        tem_transcricao, transcricao = processar_audio_de_teste(CHAMANDO_MARIA, self.reconhecedor)
-
-        self.assertTrue(tem_transcricao)
-
-        tokens = tokenizar_transcricao(transcricao)
-        self.assertIsNotNone(tokens)
-        self.assertNotEqual(tokens[0], self.nome_do_assistente)
-
-class TesteLampada(unittest.TestCase):
+class TesteRota(unittest.TestCase):
 
     def setUp(self):
         self.iniciado, self.reconhecedor, self.palavras_de_parada, self.nome_do_assistente, self.acoes = iniciar()
 
-    def testar_01_ligar_lampada(self):
-        tem_transcricao, transcricao = processar_audio_de_teste(LIGAR_LAMPADA, self.reconhecedor)
+    def testar_01_verificar_rota(self):
+        tem_transcricao, transcricao = processar_audio_de_teste(VERIFICAR_ROTA, self.reconhecedor)
 
         self.assertTrue(tem_transcricao)
 
@@ -46,8 +42,8 @@ class TesteLampada(unittest.TestCase):
         valido, _, _ = validar_comando(tokens, self.nome_do_assistente, self.acoes)
         self.assertTrue(valido)
 
-    def testar_02_desligar_lampada(self):
-        tem_transcricao, transcricao = processar_audio_de_teste(DESLIGAR_LAMPADA, self.reconhecedor)
+    def testar_02_analisar_rota(self):
+        tem_transcricao, transcricao = processar_audio_de_teste(ANALISAR_ROTA, self.reconhecedor)
 
         self.assertTrue(tem_transcricao)
 
@@ -58,13 +54,102 @@ class TesteLampada(unittest.TestCase):
         valido, _, _ = validar_comando(tokens, self.nome_do_assistente, self.acoes)
         self.assertTrue(valido)
 
+class TesteLocalizacao(unittest.TestCase):
+    
+    def setUp(self):
+        self.iniciado, self.reconhecedor, self.palavras_de_parada, self.nome_do_assistente, self.acoes = iniciar()
+
+    def testar_01_localizar_veiculo(self):
+        tem_transcricao, transcricao = processar_audio_de_teste(LOCALIZAR_VEICULO, self.reconhecedor)
+
+        self.assertTrue(tem_transcricao)
+
+        tokens = tokenizar_transcricao(transcricao)
+        self.assertIsNotNone(tokens)
+
+        tokens = eliminar_palavras_de_parada(tokens, self.palavras_de_parada)
+        valido, _, _ = validar_comando(tokens, self.nome_do_assistente, self.acoes)
+        self.assertTrue(valido)
+
+    def testar_02_encontrar_veiculo(self):
+        tem_transcricao, transcricao = processar_audio_de_teste(ENCONTRAR_VEICULO, self.reconhecedor)
+
+        self.assertTrue(tem_transcricao)
+
+        tokens = tokenizar_transcricao(transcricao)
+        self.assertIsNotNone(tokens)
+
+        tokens = eliminar_palavras_de_parada(tokens, self.palavras_de_parada)
+        valido, _, _ = validar_comando(tokens, self.nome_do_assistente, self.acoes)
+        self.assertTrue(valido)
+        
+class TesteRelatorio(unittest.TestCase):
+    
+    def setUp(self):
+        self.iniciado, self.reconhecedor, self.palavras_de_parada, self.nome_do_assistente, self.acoes = iniciar()
+
+    def testar_01_gerar_relatorio(self):
+        tem_transcricao, transcricao = processar_audio_de_teste(GERAR_RELATORIO, self.reconhecedor)
+
+        self.assertTrue(tem_transcricao)
+
+        tokens = tokenizar_transcricao(transcricao)
+        self.assertIsNotNone(tokens)
+
+        tokens = eliminar_palavras_de_parada(tokens, self.palavras_de_parada)
+        valido, _, _ = validar_comando(tokens, self.nome_do_assistente, self.acoes)
+        self.assertTrue(valido)
+
+    def testar_02_fazer_relatorio(self):
+        tem_transcricao, transcricao = processar_audio_de_teste(FAZER_RELATORIO, self.reconhecedor)
+
+        self.assertTrue(tem_transcricao)
+
+        tokens = tokenizar_transcricao(transcricao)
+        self.assertIsNotNone(tokens)
+
+        tokens = eliminar_palavras_de_parada(tokens, self.palavras_de_parada)
+        valido, _, _ = validar_comando(tokens, self.nome_do_assistente, self.acoes)
+        self.assertTrue(valido)
+        
+class TesteManutencao(unittest.TestCase):
+    
+    def setUp(self):
+        self.iniciado, self.reconhecedor, self.palavras_de_parada, self.nome_do_assistente, self.acoes = iniciar()
+
+    def testar_01_agendar_manutencao(self):
+        tem_transcricao, transcricao = processar_audio_de_teste(AGENDAR_MANUTENCAO, self.reconhecedor)
+
+        self.assertTrue(tem_transcricao)
+
+        tokens = tokenizar_transcricao(transcricao)
+        self.assertIsNotNone(tokens)
+
+        tokens = eliminar_palavras_de_parada(tokens, self.palavras_de_parada)
+        valido, _, _ = validar_comando(tokens, self.nome_do_assistente, self.acoes)
+        self.assertTrue(valido)
+
+    def testar_02_marcar_manutencao(self):
+        tem_transcricao, transcricao = processar_audio_de_teste(MARCAR_MANUTENCAO, self.reconhecedor)
+
+        self.assertTrue(tem_transcricao)
+
+        tokens = tokenizar_transcricao(transcricao)
+        self.assertIsNotNone(tokens)
+
+        tokens = eliminar_palavras_de_parada(tokens, self.palavras_de_parada)
+        valido, _, _ = validar_comando(tokens, self.nome_do_assistente, self.acoes)
+        self.assertTrue(valido)
 
 if __name__ == "__main__":
     carregador = unittest.TestLoader()
     testes = unittest.TestSuite()
 
     testes.addTest(carregador.loadTestsFromTestCase(TesteNomeAssistente))
-    testes.addTest(carregador.loadTestsFromTestCase(TesteLampada))
+    testes.addTest(carregador.loadTestsFromTestCase(TesteLocalizacao))
+    testes.addTest(carregador.loadTestsFromTestCase(TesteRelatorio))
+    testes.addTest(carregador.loadTestsFromTestCase(TesteRota))
+    testes.addTest(carregador.loadTestsFromTestCase(TesteManutencao))
 
     executor = unittest.TextTestRunner()
     executor.run(testes)
